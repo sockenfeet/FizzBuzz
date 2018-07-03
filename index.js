11ac513e2e062cb9ea286cfea41a3e2ea57d1db3
Jo Lee <jo.lee@softwire.com>
2018-07-03 17:27:19 +0100

    multiples of 13 insert "Fezz" in place

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ for (let i=1; i<101; i++) {
     if (i % 11 == 0) {
         string = "Bong";
     }
+    if (i % 13 == 0) {
+        const x = string.indexOf("B"); //index of the first instance of "B" in string
+        if (x == -1) {
+            string = "Fezz" + string;
+        } else {
+            const x = string.indexOf("B"); //index of the first instance of "B" in string
+            //console.log("X is: " + x);
+            string = string.slice(0, x) + "Fezz" + string.slice(x);
+        }
+    }
     if (string == "") {
         console.log(i);
     } else {
