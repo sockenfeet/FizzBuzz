81630de1fca27079873d9cec285516220819ea78
Jo Lee <jo.lee@softwire.com>
2018-07-04 09:19:14 +0100

    multiples of 17 reverse word order

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 for (let i=1; i<101; i++) {
-    let string = "";
+    let string = new Array(0); //empty array of relevant words
     if (i % 3 == 0) {
-        string += "Fizz";
+        string.push("Fizz");
     }
     if (i % 5 == 0) {
-        string += "Buzz";
+        string.push("Buzz");
     }
     if (i % 7 == 0) {
-        string += "Bang";
+        string.push("Bang");
     }
     if (i % 11 == 0) {
-        string = "Bong";
+        string = ["Bong"];
     }
     if (i % 13 == 0) {
-        const x = string.indexOf("B"); //index of the first instance of "B" in string
-        if (x == -1) {
-            string = "Fezz" + string;
+        const x = findBinArr(string); //index of first element of String which begins with "B"
+        if (x == string.length) { //no elements of string start with "B"
+            string.unshift("Fezz");
         } else {
-            const x = string.indexOf("B"); //index of the first instance of "B" in string
-            //console.log("X is: " + x);
-            string = string.slice(0, x) + "Fezz" + string.slice(x);
+            string.splice(x,0,"Fezz");
+            //string = string.slice(0, x) + "Fezz" + string.slice(x); //needsfixingfornewArrayStructure
         }
     }
-    if (string == "") {
+    if (i % 17 == 0) {
+        string.reverse();
+    }
+    if (string.length == 0) {
         console.log(i);
     } else {
-        console.log(string);
+        console.log(string.join(""));
     }
+}
+
+function findBinArr(str) {
+    let i=0;
+    let f=false; //flags when the relevant value is found
+    while (i<str.length && f==false) {
+        if (str[i][0] == "B") {
+            f = true;
+        } else {
+            i++;
+        }
+    }
+    return i;
 }
